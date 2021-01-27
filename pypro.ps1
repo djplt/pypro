dcfa0bc46b3e99fcc9669554ef13cd0c2b428a99
djplt <dominic.j.platt@hotmail.com>
2021-01-27 18:14:50 +0000

    Make pypro installer work without pyenv on win

--- pypro.ps1
+++ pypro.ps1
@@ -1,4 +1,44 @@
+Function Test-CommandExists
+{
+
+ Param ($command)
+
+ $oldPreference = $ErrorActionPreference
+
+ $ErrorActionPreference = "stop"
+
+ try {if(Get-Command $command){RETURN $true}}
+
+ Catch {Write-Host “$command does not exist”; RETURN $false}
+
+ Finally {$ErrorActionPreference=$oldPreference}
+
+} #end function test-CommandExists
+
 Write-Output "Welcome to python app init which uses poetry & pyenv"
+
+
+if (-Not (Test-CommandExists poetry) )
+{
+  Write-Output "Please install poetry for this script to work"
+  exit 1
+}
+
+$codeInstalled = Test-CommandExists code
+if (-Not $codeInstalled )
+{
+  Write-Output "Warning VSCode is not installed or could not be found on the path"
+}
+
+$pyenvInstalled = Test-CommandExists pyenv
+$pythonInstalled = Test-CommandExists python
+if ((-Not $pyenvInstalled) -and (-Not $pythonInstalled)) {
+  Write-Output "Warning pyenv and python is not installed"
+  exit 1
+}
+elseif (-Not $pyenvInstalled) {
+  Write-Output "Warning pyenv is not installed or could not be found on path"
+}
 
 Write-Output "`n***`n"
 $poetryV = poetry --version
@@ -21,54 +61,71 @@ Write-Output "VSCode version:"
 Write-Output $vscodeV
 
 Write-Output "`n***`n"
-$vscodeV = pyenv --version
-if ( $LASTEXITCODE -ne 0 ) 
+if ($pyenvInstalled)
 {
-  Write-Output "Pyenv command failed"
-  exit $LASTEXITCODE
-}
-Write-Output "Pyenv version:"
-Write-Output $vscodeV
+  $vscodeV = pyenv --version
+  if ( $LASTEXITCODE -ne 0 ) 
+  {
+    Write-Output "Pyenv command failed"
+    exit $LASTEXITCODE
+  }
+  Write-Output "Pyenv version:"
+  Write-Output $vscodeV
 
-$vscodeV = pyenv versions
-if ( $LASTEXITCODE -ne 0 ) 
-{
-  Write-Output "Pyenv command failed"
-  exit $LASTEXITCODE
-}
-Write-Output "Pyenv versions available:"
-Write-Output $vscodeV
+  $vscodeV = pyenv versions
+  if ( $LASTEXITCODE -ne 0 ) 
+  {
+    Write-Output "Pyenv command failed"
+    exit $LASTEXITCODE
+  }
+  Write-Output "Pyenv versions available:"
+  Write-Output $vscodeV
 
-Write-Output "`n***`n"
-$Host.UI.RawUI.FlushInputBuffer()
-$ans = Read-Host "Use current python version? [y/n]"
-$python_name = pyenv vname
-if ($ans -eq 'y')
-{
- # Pass
-}
-elseif ($ans -eq 'n')
-{
+  Write-Output "`n***`n"
   $Host.UI.RawUI.FlushInputBuffer()
-  $ans = Read-Host "Please enter a python version to use: "
-  $python_name = $ans
-}
-else 
-{
-  Write-Output "Invalid option"
-  exit 1
-}
+  $ans = Read-Host "Use current python version? [y/n]"
+  $python_name = pyenv vname
+  if ($ans -eq 'y')
+  {
+  # Pass
+  }
+  elseif ($ans -eq 'n')
+  {
+    $Host.UI.RawUI.FlushInputBuffer()
+    $ans = Read-Host "Please enter a python version to use: "
+    $python_name = $ans
+  }
+  else 
+  {
+    Write-Output "Invalid option"
+    exit 1
+  }
 
-pyenv rehash
-Write-Output "Using python version $python_name"
+  pyenv rehash
+  Write-Output "Using python version $python_name"
 
-pyenv local $python_name
-if ( $LASTEXITCODE -ne 0 ) 
-{
-  Write-Output "Pyenv command failed"
-  exit $LASTEXITCODE
+  pyenv local $python_name
+  if ( $LASTEXITCODE -ne 0 ) 
+  {
+    Write-Output "Pyenv command failed"
+    exit $LASTEXITCODE
+  }
 }
+else {
+  Write-Output "Current python version is $(python -V)"
+  Write-Output "Current python path is $(where.exe python)"
+  $currentPython = "python"
+  $Host.UI.RawUI.FlushInputBuffer()
+  $response = Read-Host "Use this python version?"
+  if ($ans -eq 'y') {
+    # Pass
+  }
+  else {
+    $Host.UI.RawUI.FlushInputBuffer()
+    $currentPython = Read-Host "Please type in the python executable to use (or full path to python if not in path)"
+  }
 
+}
 
 Write-Output "`n***`n"
 $Host.UI.RawUI.FlushInputBuffer()
@@ -76,8 +133,20 @@ $proj = Read-Host "Please enter a project name: "
 
 poetry new $proj
 Set-Location $proj
-pyenv local $python_name
-pyenv rehash
+
+$didPythonPathGetSetCorrectly = 1
+if ($pyenvInstalled) {
+  pyenv local $python_name
+  pyenv rehash
+}
+else {
+  poetry env use $currentPython
+}
+
+if ($LASTEXITCODE -ne 0) {
+  Write-Output "Warning: problem setting poetry python environment"
+  $didPythonPathGetSetCorrectly = 0
+}
 
 $Host.UI.RawUI.FlushInputBuffer()
 $ans = Read-Host "Install venv? [y/n]"
@@ -119,4 +188,8 @@ $ans = Read-Host "Open VSCode?"
 if ( $response -eq 'y' )
 {
   code .
+}
+
+if ($didPythonPathGetSetCorrectly -eq 0) {
+  Write-Output "error setting poetry python executable, you can try `npoetry env use python_exe"
 }
